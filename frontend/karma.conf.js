2c7a7182afd6fba926c3105146d4f2611c5f10a9
Daan Van den Nest <daanvdn@gmail.com>
2025-12-22 12:24:07 +0100

    frontend more fixes for running tests

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
